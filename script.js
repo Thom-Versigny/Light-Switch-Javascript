97fc7d850d13cb14c738ba19b90df0d4c69dd8ae
Thom Versigny <Thomversigny@hotmail.com>
2022-03-15 09:48:05 +0100

    opdracht voltooid

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'licht knopje';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+var x = 0;
+btn.onclick = lightaan
+function lightaan(){
+if (x == 0) {
+    console.log("licht staat aan");
+    document.body.style.backgroundColor = "yellow";
+    btn.innerHTML = 'licht knopje staat aan';
+    x = 1
+} else {
+    console.log("licht staat uit");
+    document.body.style.backgroundColor = "black";
+    btn.innerHTML = 'licht knopje staat uit';
+    x = 0
+}
 
-
-
+}
 // schijf hier tussen je code
